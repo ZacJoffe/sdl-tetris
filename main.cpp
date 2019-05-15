a161f4434d3ad3abe3e552e7d3fda87d02080bdf
ZacJoffe <zacharyjoffe@gmail.com>
2019-05-14 20:32:39 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <SDL2/SDL.h>
+
+#define WIDTH 22
+#define HEIGHT 10
+
+class Piece {
+private:
+
+public:
+    Piece() {
+
+    }
+
+    ~Piece() {
+
+    }
+
+    bool Rotate() {
+
+    }
+};
+
+class Board {
+private:
+    int board[WIDTH][HEIGHT];
+    //Piece piece;
+
+public:
+    Board() {
+        
+    }
+
+    ~Board() {
+
+    }
+};
